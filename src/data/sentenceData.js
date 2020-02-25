721beaed682eba083defa452aafa77e421387543
johntr <jtredlich@gmail.com>
2020-02-25 17:30:28 -0500

    Added test data

---
+++ src/data/sentenceData.js
@@ -0,0 +1,12 @@
+const data = [
+  {
+    phrase: `Today I had a | day at school!`,
+    choices: ["good", "bad"]
+  },
+  {
+    phrase: `My special today was | . I had | .`,
+    choices: ["Gym", "Library", "Art", "Music", "Fun", "a bad time"]
+  }
+];
+
+export default data;
