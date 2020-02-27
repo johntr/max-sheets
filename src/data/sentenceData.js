b6ed70d2b853f4f492b28f111b2b2906c19797ff
johntr <jtredlich@gmail.com>
2020-02-26 22:29:03 -0500

    updated questions

--- src/data/sentenceData.js
+++ src/data/sentenceData.js
@@ -1,11 +1,17 @@
 const data = [
   {
     phrase: `Today I had a | day at school!`,
-    choices: ["good", "bad"]
+    choices: [["good", "bad", "okay"]]
   },
   {
-    phrase: `My special today was | . I had | .`,
-    choices: ["Gym", "Library", "Art", "Music", "Fun", "a bad time"]
+    phrase: `My special today was | . In my special we | , | , | . I had | .`,
+    choices: [
+      ["Gym", "Library", "Art", "Music"],
+      ["Rode on a scooter", "danced", "did a warm up", "played ball"],
+      ["shopped for a book", "read a book", "listened to a story"],
+      ["used glue", "made a picture", "painted", "cut paper"],
+      ["played an instrument", "sang", "drummed"]
+    ]
   }
 ];
 
